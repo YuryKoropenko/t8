137cbcc96afb8e0d0d5608ef9ed8f66ea0e1b9e7
YuryKoropenko <yurykoropenko@gmail.com>
2017-11-09 17:14:39 +0200

    главная страница (все всплывашки на главной + 2 мобильных меню)

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function() {
+	$(window).resize(function() {
+		if ( $(window).width() < 767 ) {
+			$('.header-desktop').hide();
+			$('.header-mob').show();
+		}
+		if ( $(window).width() > 767 ) {
+			$('.header-desktop').show();
+			$('.header-mob').hide();
+		}
+	});
+
 	$('.p-slider').owlCarousel({
 		loop:true,
 		margin:0,
@@ -17,4 +28,53 @@ $(function() {
 		$(this).parent().parent().parent().hide();
 		return false;
 	});
+
+	$('.h-profil__corz').on('click', function() {
+		$('.h-profilPopup').fadeToggle(100);
+		$(document).click(function(event) {
+			if ($(event.target).closest(".h-profilPopup").length) return;
+				$('.h-profilPopup').fadeOut(100);
+				event.stopPropagation();
+			});
+		return false;
+	});
+
+	$('.mob-menu__button').on('click', function() {
+		$(this).parent().children('.mob-menu').slideToggle(300);
+		$(document).click(function(event) {
+			if ($(event.target).closest(".mob-menu").length) return;
+				$('.mob-menu').slideUp(300);
+				event.stopPropagation();
+			});
+		return false;
+	});
+
+	$('.mob-menu__link').on('click', function() {
+		$(this).parent('.mob-menu__item-menu').children('.mob-menuSub').stop(false, true).slideToggle(300);
+	});
+
+	$('.mob-catalogmenu__button').on('click', function() {
+		$(this).parent().children('.mob-catalogmenu').stop(false, true).slideToggle(300);
+		$(document).click(function(event) {
+			if ($(event.target).closest(".mob-catalogmenu").length) return;
+				$('.mob-catalogmenu').slideUp(300);
+				event.stopPropagation();
+			});
+		return false;
+	});
+
+	$('.mob-catalogmenu__link').on('click', function() {
+		$(this).parent('.mob-catalogmenu__item-menu').children('.mob-catalogmenuSub').stop(false, true).slideToggle(300);
+	});
+
+	$('.h-catalog__item').hover(function() {
+		$(this).children('.h-catalog-sub').stop(false, true).fadeIn(100);
+	}, function() {
+		$(this).children('.h-catalog-sub').stop(false, true).fadeOut(100);
+	});
+
+	$('.nohref').on('click', function() {
+		return false;
+	});
+
 });
